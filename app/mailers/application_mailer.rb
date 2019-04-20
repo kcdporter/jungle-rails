36b9947bb2ecbecfce75dcf0ad7716dbf93b8298
kcdporter <kiracdporter@gmail.com>
2019-04-20 21:22:18 +0000

    restarting mailer branch

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@jungle.com"
   layout 'mailer'
+
+  def order_total(order)
+    order.map {humanized_money_with_symbol line_item.total_price}
+  end
 end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,14 @@
 class OrderMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
-  def order_email
-    #this url needs to be http://localhost:3000/orders/:id
-    @url  = 'http://localhost:3000/orders/:id'
-    mail(to:@order.email subject: 'Thank you for your order!')
+
+  def order_email(order)
+    @order = order
+    @url  = 'http://localhost:3000/'
+    mail(to:@order.email, subject: 'Thank you for your order!')
   end
+
+  def order_total
+    @order.map {humanized_money_with_symbol line_item.total_price}
+  end
+  helper_method :order_total
 end
